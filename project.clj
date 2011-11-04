c370d90859f23a8917df79a8fc33bddcd4f90aa3
Jake McCrary <jake@jakemccrary.com>
2011-11-04 16:12:59 -0500

    Stop using deprecated packages.
    Keep project modern. Don't use old leiningen packages.

--- project.clj
+++ project.clj
@@ -1,7 +1,4 @@
-(defproject lein-fig/lein-fig "1.0.3"
+(defproject lein-fig/lein-fig "1.0.4"
   :description "Collect fig dependencies alongside your maven dependencies."
-  
   :eval-in-leiningen true
-  :min-lein-version "1.4.0"
-
   :dev-dependencies [[lein-clojars "0.6.0"]])
